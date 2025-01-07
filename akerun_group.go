07c26075ba04ee69d84bb81e7455f24f09bb455a
hayao <shun819.mail@gmail.com>
2025-01-08 02:27:12 +0900

    :feat: add group methods

--- akerun_group.go
+++ akerun_group.go
@@ -3,8 +3,10 @@ package akerun
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"path"
 
+	"github.com/google/go-querystring/query"
 	"golang.org/x/oauth2"
 )
 
@@ -32,6 +34,9 @@ type AkerunGroupList struct {
 }
 
 type akerunGroupRow struct {
+	AkerunGroup AkerunGroup `json:"akerun_group"`
+}
+type akerunGroupDetailedRow struct {
 	AkerunGroup AkerunGroupDetailed `json:"akerun_group"`
 }
 
@@ -54,10 +59,112 @@ func (c *Client) GetAkerunGroup(
 	organizationId string,
 	akerunGroupId string,
 ) (*AkerunGroupDetailed, error) {
-	var result akerunGroupRow
+	var result akerunGroupDetailedRow
 	err := c.callVersion(ctx, path.Join(apiPathOrganizations, organizationId, apiPathAkerunGroup, akerunGroupId), http.MethodGet, oauth2Token, nil, nil, &result)
 	if err != nil {
 		return nil, err
 	}
 	return &result.AkerunGroup, nil
+}
+
+type AkerunGroupCreateParameter struct {
+	Name string `url:"name"`
+	Memo string `url:"memo"`
+}
+
+func (c *Client) CreateAkerunGroup(
+	ctx context.Context,
+	oauth2Token *oauth2.Token,
+	organizationId string,
+	params AkerunGroupCreateParameter,
+) (*AkerunGroup, error) {
+	var result akerunGroupRow
+	v, err := query.Values(params)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.callVersion(ctx, path.Join(apiPathOrganizations, organizationId, apiPathAkerunGroup), http.MethodPost, oauth2Token, v, nil, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result.AkerunGroup, nil
+}
+
+type AkerunGroupUpdateParameter struct {
+	Name string `url:"name"`
+	Memo string `url:"memo"`
+}
+
+func (c *Client) UpdateAkerunGroup(
+	ctx context.Context,
+	oauth2Token *oauth2.Token,
+	organizationId string,
+	akerunGroupId string,
+	params AkerunGroupUpdateParameter,
+) (*AkerunGroup, error) {
+	var result akerunGroupRow
+	v, err := query.Values(params)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.callVersion(ctx, path.Join(apiPathOrganizations, organizationId, apiPathAkerunGroup, akerunGroupId), http.MethodPut, oauth2Token, v, nil, &result)
+	if err != nil {
+		return nil, err
+	}
+	return &result.AkerunGroup, nil
+}
+
+func (c *Client) DeleteAkerunGroup(
+	ctx context.Context,
+	oauth2Token *oauth2.Token,
+	organizationId string,
+	akerunGroupId string,
+) error {
+	err := c.callVersion(ctx, path.Join(apiPathOrganizations, organizationId, apiPathAkerunGroup, akerunGroupId), http.MethodDelete, oauth2Token, nil, nil, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) AddAkerunToGroup(
+	ctx context.Context,
+	oauth2Token *oauth2.Token,
+	organizationId string,
+	akerunGroupId string,
+	akerunIds ...string,
+) error {
+	var result akerunGroupDetailedRow
+	v := url.Values{}
+	for _, id := range akerunIds {
+		v.Add("akerun_ids[]", id)
+	}
+
+	err := c.callVersion(ctx, path.Join(apiPathOrganizations, organizationId, apiPathAkerunGroup, akerunGroupId, "akeruns"), http.MethodPost, oauth2Token, v, nil, &result)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) RemoveAkerunFromGroup(
+	ctx context.Context,
+	oauth2Token *oauth2.Token,
+	organizationId string,
+	akerunGroupId string,
+	akerunIds ...string,
+) error {
+	var result akerunGroupDetailedRow
+	v := url.Values{}
+	for _, id := range akerunIds {
+		v.Add("akerun_ids[]", id)
+	}
+
+	err := c.callVersion(ctx, path.Join(apiPathOrganizations, organizationId, apiPathAkerunGroup, akerunGroupId, "akeruns"), http.MethodDelete, oauth2Token, v, nil, &result)
+	if err != nil {
+		return err
+	}
+	return nil
 }
